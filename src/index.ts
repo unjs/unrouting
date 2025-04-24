2d84c14f89cbb539b5f8837635ab569586dcf4c9
Daniel Roe <daniel@roe.dev>
2025-04-24 21:50:51 +0200

    chore: lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { toRadix3, toRegExp, toVueRouter4 } from './converters'
 
-export type { ParsePathOptions, ParsedPath, ParsedPathSegment, ParsedPathSegmentToken, SegmentType } from './parse'
+export type { ParsedPath, ParsedPathSegment, ParsedPathSegmentToken, ParsePathOptions, SegmentType } from './parse'
 export { parsePath } from './parse'
